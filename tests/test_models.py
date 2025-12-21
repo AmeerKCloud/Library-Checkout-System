51fe3ed2b21bb596884741ca11dac1b2645335ee
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-12-21 12:55:56 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 # Here go all of the prototype classes in their testing phase
 # before i post them in the final file.
 
-class Library:
+class Book:
     def __init__(self, title, author):
         pass
 
