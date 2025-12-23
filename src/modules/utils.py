162d9f3826d4c0d6b00469e7fcc4f1b6df6341c4
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-12-23 10:41:23 -0500

    Update utils.py

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -5,6 +5,9 @@ def get_project_root():
     return os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
+def get_data_file():
+    return os.path.join(get_project_root(), "data", "library_books.json")
 
+print(get_data_file())
 
 
