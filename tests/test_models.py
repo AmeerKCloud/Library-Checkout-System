507d6823926cf1b0a361e20c89316686cd524f05
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-12-23 13:04:04 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,21 +1,34 @@
 # Here go all of the prototype classes in their testing phase
 # before i post them in the final file.
 
-class Book:
-    def __init__(self, title, author):
-        self.title = title
-        self.author = author
+import json
+from src.modules.utils import get_data_file
 
-    def is_available(self):
-        pass
+def test_load_books():
+    json_file_path = get_data_file("library_books.json")
+
+    with open(json_file_path, "r") as file:
+        books = json.load(file)
+
+    assert len(books) > 0
 
 
-class User:
-    def __init__(self):
-        self.borrowed_books = {}
 
-    def borrow(self):
-        pass
+# class Book:
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
 
-    def return_book(self):
-        pass
+#     def is_available(self):
+#         pass
+
+
+# class User:
+#     def __init__(self):
+#         self.borrowed_books = {}
+
+#     def borrow(self):
+#         pass
+
+#     def return_book(self):
+#         pass
