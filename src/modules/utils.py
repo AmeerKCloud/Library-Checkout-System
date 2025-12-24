876ff2bda72d3e2080f4902e278e30781e1beea1
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-12-24 12:00:01 -0500

    Update utils.py

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -8,7 +8,7 @@ import os
 def get_project_root():
     return os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-#⬇️ This function gets imported & invoked in the other files, with name of intended file in place of paremeter 'filename'
+#⬇️ This function gets imported & invoked in other files, with name of intended file in place of paremeter 'filename'
 def get_data_file(filename):
     return os.path.join(get_project_root(), "data", filename)
 
