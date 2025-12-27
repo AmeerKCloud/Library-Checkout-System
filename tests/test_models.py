c198812272cb77a07b1b54464f481575ab05766a
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-12-26 20:55:00 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -19,7 +19,7 @@ class Library:
 
     def borrow_book(self):
         for item in self.json_books_data_list:
-            if self.book_title in item.values() and self.book_author in item.values():
+            if self.book_title in item.values() and self.book_author in item.values():      #⬅️ '.values()' specifically seeks dict. values only
                 print(f"Title: {self.book_title}, and Author: {self.book_author}, exist.")
         pass
 
