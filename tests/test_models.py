3594e26f892f68507768aea921cb725459407398
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-12-28 17:04:06 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -34,7 +34,7 @@ class Library:
 
                     for key, value in item.items():
                         print(f"{key}: {value}")
-                    return item["book_id"], self.json_books_data_list
+                    return item["book_id"], item["available"]
 
     def return_book(self):
         pass
