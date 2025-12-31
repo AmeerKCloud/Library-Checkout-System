7309ebaa35bf7dafdbfe786bdd0b47f5659a3ad0
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-12-31 10:09:29 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -94,6 +94,7 @@ while True:
         author = input("\nAuthors name:\n")
 
         library = Library(title, author, test_load_books())
+        library.return_book()
     elif user_option_choice == 'v':
         library.show_books()
     else:
