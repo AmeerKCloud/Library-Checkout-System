aefbcdacb49b68366a8236d12ad149cb2ec01b68
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-01-04 11:16:24 -0500

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -75,7 +75,7 @@ class User:                                                             #⬅️ 
         self.user_books_data = {}                                              #⬅️ The main dict., user name as key, list of dict. items as value.
 
     def borrow_books(self):
-        self.borrowed_books_list = []
+        self.borrowed_books_list = []                                          #⬅️ The empty list.
         self.user_books_data[self.u_name] = self.borrowed_books_list           #⬅️ The list as a value in the main dict.
         self.book = {}                                                         #⬅️ The list item dict.
 
