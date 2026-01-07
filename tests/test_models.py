0add93cad3bdacc66432045699e8e75d8f8e0717
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-01-06 19:03:41 -0500

    updates

--- tests/test_models.py
+++ tests/test_models.py
@@ -72,17 +72,16 @@ class User:                                                             #⬅️ 
         self.json_books_data_list = json_books_data_list
         self.user_books_data = {}                                              #⬅️ The main dict., user name as key, list of dict. items as value.
 
-        # if self.user_books_data:
-        #     for key in self.user_books_data:
-        #         if key != self.u_name:                       #⬅️ If user not already in dict. data, then create username key & assign list as value
-        #             print(f"\n{self.u_name} just added.")
-        #             self.user_books_data[self.u_name] = []                                 #⬅️ Create the user's list ONCE.
+        if self.user_books_data:
+            for key in self.user_books_data:
+                if key != self.u_name:                       #⬅️ If user not already in dict. data, then create username key & assign list as value
+                    print(f"\n{self.u_name} just added.")
+                    self.user_books_data[self.u_name] = []                                 #⬅️ Create the user's list ONCE.
+        else:
+            self.user_books_data[self.u_name] = []
 
     def borrow_books(self, title, author):
-        if not self.user_books_data:
-            print(f"\n{self.u_name} just added.")
-            self.user_books_data[self.u_name] = [] 
-        elif self.u_name in self.user_books_data:
+        if self.u_name in self.user_books_data:
             for item in self.json_books_data_list:
                 if item["title"] == title and item["author"] == author:
                     # Create a NEW book dict each time
