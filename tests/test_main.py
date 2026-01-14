571c57524d2f2e98596a7cbd16e22776e69e7702
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-01-14 12:24:39 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -171,10 +171,10 @@ while True:
 # - Refactor code to improve modularity and separation of concerns
 # - Add error handling for file operations and user inputs
 
-# Currently program does not catch irrelevant user inputs, like random gibberish
+# [❌ in progress] Currently program does not catch irrelevant user inputs, like random gibberish
 # - Create functionality that checks for this in the 'test_utils.py' file
 
-# Currently user is unable to view their borrowed or returned books history
+# [✅Resolved] Currently user is unable to view their borrowed or returned books history
 # - Give the 'test_menu.py' program functionality that allows this
 # - Perhaps an if/else statement that lets the user choose from two options:
 # - - Option 'a' lets him create a new transaction (the already existed menu)
